fa9b4759040b81925fabe974985fac0b3940579d
Marge C. <marge.consunji@gmail.com>
2024-05-31 16:37:00 +0800

    Create inner timer circle

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,13 @@
-<script setup></script>
+<script setup>
+import { ref } from 'vue'
+
+const minutes = ref()
+
+// function setMinutes(mins) {
+//   console.log(mins)
+//   return;
+// }
+</script>
 
 <template>
   <div id="main-app">
@@ -7,8 +16,19 @@
     </header>
 
     <main>
-      <div>Set _ minutes <button>go</button></div>
-      <div id="timer"></div>
+      <div>
+        <p>Set minutes:</p>
+        <input type="number" v-model="minutes" placeholder="0" min="0" max="60" step="1" />
+        <button>Go</button>
+      </div>
+      <div id="timer">
+        <div class="timer-inner"></div>
+      </div>
+      <p>time is {{ minutes }}</p>
+      <div>
+        <button>Stop</button>
+        <button>Clear</button>
+      </div>
     </main>
   </div>
 </template>
